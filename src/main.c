bb3e43fec2b2f6898e3e8a219eeb361b333ea24b
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-07-15 08:52:15 -0400

    length function

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "stringlib.h"
+
+int main() {
+    // add tests here
+    char* test = "hello_world";
+    printf("%d", len(test));
+    return 0;
+}
---
+++ src/stringlib.c
@@ -0,0 +1,5 @@
+int len(char* str) {
+    int i = 0;
+    while (str[i] != '\0') i++;
+    return i;
+}
